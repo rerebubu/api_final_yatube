7a19532f5dfcfa12dad70784f8f3a7281e5da094
Егор Борушкин <redbuuuulle1@gmail.com>
2025-03-27 20:42:10 +0300

    28

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,16 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # Разрешаем только авторизованным пользователям делать POST, PUT,
+        # DELETE
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user == obj.author)
+        # Разрешаем GET, HEAD, OPTIONS всем
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Только автор может изменять или удалять объект
+        return request.user == obj.author
